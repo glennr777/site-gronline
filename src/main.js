f52420b96f160baa8173b9c6c73bdbb312a7346f
glennr777 <glennr777@gmail.com>
2023-11-19 14:10:39 +0000

    ES2020 update

--- src/main.js
+++ src/main.js
@@ -1,38 +1,31 @@
 import "./main.css"
 
-const mainNav = document.getElementById('mainNav');
-const navBar = document.getElementById('navBar');
-const navButton = document.getElementById('navButton');
-
-window.toggleMenu = (to) => {
-  let set = to;
-  if (to === undefined) {
-    set = navBar.ariaHidden === 'true';
-  }
-  
+const toggleMenu = (to) => {
+  const navBar = document.getElementById('navBar');
+  let set = to === undefined ? navBar.ariaHidden === 'true' : to;  
   navBar.ariaHidden = set ? 'false' : 'true';
   navBar.tabIndex = set ? 0 : -1;
 };
 
 const menuClick = (e) => {
-  if (e && e.preventDefault) e.preventDefault();
-  if (e && e.stopImmediatePropagation) e.stopImmediatePropagation();
+  e.preventDefault?.();
+  e.stopImmediatePropagation?.();
   toggleMenu();
 };
 
 const updateScroll = () => {
   toggleMenu(document.body.offsetWidth > 480);
-  mainNav.classList.toggle('scrolled', window.scrollY > 0);
+  document.getElementById('mainNav').classList.toggle('scrolled', window.scrollY > 0);
 };
 
 const debouceScroll = () => {
   requestAnimationFrame(updateScroll);
 };
 
-navButton.addEventListener('click', menuClick);
+document.getElementById('navButton').addEventListener('click', menuClick);
+document.body.parentNode.classList.add('ready');
 
 updateScroll();
 
-document.body.parentNode.classList.add('ready');
 window.addEventListener('scroll', debouceScroll);
 window.addEventListener('resize', debouceScroll);
