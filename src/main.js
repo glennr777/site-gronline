f89f3dfec0549a48ecc05c8a1046ec6a660496e2
glennr777 <glennr777@gmail.com>
2023-11-19 13:36:12 +0000

    fix accessibility on nav

--- src/main.js
+++ src/main.js
@@ -1,38 +1,38 @@
-import './main.css';
+import "./main.css"
 
-{
-  const mainNav = document.getElementById('mainNav');
-  const navBar = document.getElementById('navBar');
-  const navButton = document.getElementById('navButton');
+const mainNav = document.getElementById('mainNav');
+const navBar = document.getElementById('navBar');
+const navButton = document.getElementById('navButton');
 
-  const toggleMenu = (to) => {
-    const set = to !== undefined ? to : navBar.ariaHidden === 'true';
-    navBar.ariaHidden = set ? '' : 'true';
-    navBar.tabIndex = set ? 0 : -1;
-  };
-
-  const menuClick = (e) => {
-    if (e && e.preventDefault) e.preventDefault();
-    if (e && e.stopImmediatePropagation) e.stopImmediatePropagation();
-    toggleMenu();
-  };
-
-  const updateScroll = () => {
-    toggleMenu(false);
-    mainNav.classList.toggle('scrolled', window.scrollY > 0);
-  };
-
-  const debouceScroll = () => {
-    requestAnimationFrame(updateScroll);
-  };
-
-  navButton.addEventListener('click', menuClick);
-  
-  if (document.body.offsetWidth <= 480) {
-    toggleMenu(false);
+window.toggleMenu = (to) => {
+  let set = to;
+  if (to === undefined) {
+    set = navBar.ariaHidden === 'true';
   }
   
-  updateScroll();
-  document.body.parentNode.classList.add('ready');
-  window.addEventListener('scroll', debouceScroll);
+  navBar.ariaHidden = set ? 'false' : 'true';
+  navBar.tabIndex = set ? 0 : -1;
 };
+
+const menuClick = (e) => {
+  if (e && e.preventDefault) e.preventDefault();
+  if (e && e.stopImmediatePropagation) e.stopImmediatePropagation();
+  toggleMenu();
+};
+
+const updateScroll = () => {
+  toggleMenu(document.body.offsetWidth > 480);
+  mainNav.classList.toggle('scrolled', window.scrollY > 0);
+};
+
+const debouceScroll = () => {
+  requestAnimationFrame(updateScroll);
+};
+
+navButton.addEventListener('click', menuClick);
+
+updateScroll();
+
+document.body.parentNode.classList.add('ready');
+window.addEventListener('scroll', debouceScroll);
+window.addEventListener('resize', debouceScroll);
